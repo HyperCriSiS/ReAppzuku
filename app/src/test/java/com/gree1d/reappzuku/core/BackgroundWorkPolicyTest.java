19d542b6722331dc2b9b2ebf0df5dc25e3729e3b
HyperCriSiS <dev.github@w3b.world>
2026-09-05 18:14:18 +0200

    Exhaustively test App Behavior continuity truth table

--- app/src/test/java/com/gree1d/reappzuku/core/BackgroundWorkPolicyTest.java
+++ app/src/test/java/com/gree1d/reappzuku/core/BackgroundWorkPolicyTest.java
@@ -9,25 +9,46 @@ import static org.junit.Assert.assertEquals;
 public class BackgroundWorkPolicyTest {
 
     @Test
-    public void noAutomationProducesNoBlockers() {
-        assertEquals(
-                EnumSet.noneOf(BackgroundWorkPolicy.Blocker.class),
-                BackgroundWorkPolicy.resolveActiveBlockers(false, false, false, false, false));
-    }
+    public void exhaustiveContinuityTruthTableMatchesBlockerSet() {
+        BackgroundWorkPolicy.Blocker[] blockerByBit = {
+                BackgroundWorkPolicy.Blocker.AUTO_KILL,
+                BackgroundWorkPolicy.Blocker.SMART_LIFECYCLE,
+                BackgroundWorkPolicy.Blocker.SLEEP_MODE,
+                BackgroundWorkPolicy.Blocker.ACTIVE_PRESET,
+                BackgroundWorkPolicy.Blocker.RESTRICTIONS_SCHEDULE
+        };
 
-    @Test
-    public void reportsEveryActiveContinuityRequirement() {
-        assertEquals(
-                EnumSet.allOf(BackgroundWorkPolicy.Blocker.class),
-                BackgroundWorkPolicy.resolveActiveBlockers(true, true, true, true, true));
-    }
+        for (int mask = 0; mask < (1 << blockerByBit.length); mask++) {
+            boolean autoKill = (mask & 1) != 0;
+            boolean smartLifecycle = (mask & 2) != 0;
+            boolean sleepMode = (mask & 4) != 0;
+            boolean activePreset = (mask & 8) != 0;
+            boolean restrictionSchedule = (mask & 16) != 0;
 
-    @Test
-    public void reportsOnlyEnabledBlockers() {
-        assertEquals(
-                EnumSet.of(
-                        BackgroundWorkPolicy.Blocker.SMART_LIFECYCLE,
-                        BackgroundWorkPolicy.Blocker.RESTRICTIONS_SCHEDULE),
-                BackgroundWorkPolicy.resolveActiveBlockers(false, true, false, false, true));
+            EnumSet<BackgroundWorkPolicy.Blocker> expected =
+                    EnumSet.noneOf(BackgroundWorkPolicy.Blocker.class);
+            for (int bit = 0; bit < blockerByBit.length; bit++) {
+                if ((mask & (1 << bit)) != 0) expected.add(blockerByBit[bit]);
+            }
+
+            EnumSet<BackgroundWorkPolicy.Blocker> actual =
+                    BackgroundWorkPolicy.resolveActiveBlockers(
+                            autoKill,
+                            smartLifecycle,
+                            sleepMode,
+                            activePreset,
+                            restrictionSchedule);
+
+            assertEquals("blocker mismatch for mask=" + mask, expected, actual);
+            assertEquals(
+                    "continuity mismatch for mask=" + mask,
+                    mask != 0,
+                    AutomationDesiredState.requiresBackgroundContinuity(
+                            autoKill,
+                            smartLifecycle,
+                            sleepMode,
+                            activePreset,
+                            restrictionSchedule));
+        }
     }
 }
